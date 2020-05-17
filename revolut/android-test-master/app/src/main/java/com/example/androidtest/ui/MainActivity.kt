a051e2ac03298195c5fe8684bcbdc69a64cf66eb
Jaggrat Singh <jaggrat.singh@skyscanner.net>
2020-05-17 23:29:18 +0100

    remove unused code

--- revolut/android-test-master/app/src/main/java/com/example/androidtest/ui/MainActivity.kt
+++ revolut/android-test-master/app/src/main/java/com/example/androidtest/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.androidtest.ui
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
